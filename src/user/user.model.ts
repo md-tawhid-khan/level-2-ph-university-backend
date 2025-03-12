0f87eaa4c80d334f7772c28aa43ce65d582431ed
md1300 <md.tawhid.khan1998@gmail.com>
2025-03-12 23:21:55 +0600

    work  user validation

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -4,7 +4,7 @@ import { TUser } from "./user.interface";
 const userSchema= new Schema<TUser>({
     id:{type:String,required:true},
     password:{type:String,required:true},
-    needChangePassword:{type:Boolean,required:true},
+    needChangePassword:{type:Boolean,},
     role:{type:String, enum:['admin','faculty','student'],required:true},
     status:{type:String, enum:['in-progress','blocked'],requirde:true},
     isDeleted:{type:Boolean,default:false}
