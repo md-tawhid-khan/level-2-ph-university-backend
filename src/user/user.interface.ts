bf721c59c3c6aa01a38925e41395bd75c770f6d9
md1300 <md.tawhid.khan1998@gmail.com>
2025-03-12 23:07:43 +0600

    set-up-project and working about User

---
+++ src/user/user.interface.ts
@@ -0,0 +1,8 @@
+ export type TUser ={
+      id:string,
+      password:string,
+      needChangePassword:boolean,
+      role:'admin'|'faculty'|'student',
+      status:'in-progress'|'blocked',
+      isDeleted:boolean,
+ }
---
+++ src/user/user.model.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from "mongoose";
+import { TUser } from "./user.interface";
+
+const userSchema= new Schema<TUser>({
+    id:{type:String,required:true},
+    password:{type:String,required:true},
+    needChangePassword:{type:Boolean,required:true},
+    role:{type:String, enum:['admin','faculty','student'],required:true},
+    status:{type:String, enum:['in-progress','blocked'],requirde:true},
+    isDeleted:{type:Boolean,default:false}
+},
+{
+    timestamps:true
+})
+
+ export const User =model('user',userSchema)
+ 
+ 
