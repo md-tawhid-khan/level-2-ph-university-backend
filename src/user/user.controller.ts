738ff8f1b7b218e04828d4c7dc42c8e43750c168
md1300 <md.tawhid.khan1998@gmail.com>
2025-03-14 21:00:45 +0600

    create sendResponse Utily

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,23 @@
+import { status } from 'http-status';
 import { NextFunction, Request, Response } from "express";
 import { userServices } from "./user.service";
+import sendResponse from "../utily/sendResponse";
 
 const createStudent=async(req:Request,res:Response,next:NextFunction)=>{
     try {
         const {password,student:studentData}=req.body;
 
         const result=await userServices.createStudentIntoDB(password,studentData)
-        res.json({
-           status:true,
-           message:'successfully create user',
-           data:result
+        // res.json({
+        //    status:true,
+        //    message:'successfully create user',
+        //    data:result
+        // })
+        sendResponse(res,{
+            statusCode:status.OK,
+            success:true,
+            message:'Student is retrived successfully',
+            data:result
         })
    }
      catch (error) {
